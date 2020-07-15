6240c225996bd3cb2d652ef3128ddb0fa14513ad
Lord Sayur A <umar_317@hotmail.com>
2020-07-16 07:23:34 +0800

    Make bullet move on instantiation

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Bullet : MonoBehaviour
+{
+    [SerializeField]
+    private float _bulletSpeed = 20f;
+    private Rigidbody rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        rigidbody.velocity = new Vector3(0, 0, _bulletSpeed);
+    }
+}
