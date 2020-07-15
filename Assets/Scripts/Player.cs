e709684e1d0438c7057cb9d625ea8c9c26382026
Lord Sayur A <umar_317@hotmail.com>
2020-07-15 11:23:27 +0800

    Implement player movement clamping

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,27 +4,91 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    private float movementSpeed = 10f;
+    private float _movementSpeed = 10f;
+    [SerializeField]
+    private bool _clampMovement = true;
+    [SerializeField]
+    private Vector2 padding;
 
-    private Vector2 movementDirection;
+    private Vector2 _movementDirection;
+    private ScreenClamp screenClamp;
+
+    private void Awake()
+    {
+        SetBoundary();
+    }
 
     private void Update()
     {
-        Move(movementDirection.y, movementDirection.x, movementSpeed);
+        Move(_movementDirection.y, _movementDirection.x, _movementSpeed, _clampMovement);
     }
 
     public void GetAxis(InputAction.CallbackContext context)
     {
-        movementDirection = context.ReadValue<Vector2>();
+        _movementDirection = context.ReadValue<Vector2>();
     }
 
-    private void Move(float forward, float sideway, float speed)
+    private void Move(float forward, float sideway, float speed, bool isClamped = true)
     {
-        transform.position = new Vector3(
-                    transform.position.x + sideway * speed * Time.deltaTime,
-                    transform.position.y,
-                    transform.position.z + forward * speed * Time.deltaTime
-                    );
+        float newForward, newSideway;
+
+        float deltaForward = forward * speed * Time.deltaTime;
+        float deltaSideway = sideway * speed * Time.deltaTime;
+
+        Vector3 newPosition;
+
+        if (isClamped)
+        {
+            newForward = Mathf.Clamp(
+                transform.position.z + deltaForward, 
+                screenClamp.LeftBottom.y, 
+                screenClamp.RightTop.y);
+            
+            newSideway = Mathf.Clamp(
+                transform.position.x + deltaSideway, 
+                screenClamp.LeftBottom.x, 
+                screenClamp.RightTop.x);
+
+            //newPosition = mainCamera.transform.TransformPoint(
+            //    new Vector3(newForward, newSideway, localPos.z));
+
+            newPosition = new Vector3(newSideway, transform.position.y, newForward);
+        }
+        else
+        {
+            newForward = transform.position.z + forward * speed * Time.deltaTime;
+            newSideway = transform.position.x + sideway * speed * Time.deltaTime;
+
+            newPosition = new Vector3(newSideway, transform.position.y, newForward);
+        };
+
+        transform.position = newPosition;
     }
 
+    private void SetBoundary()
+    {
+        var mainCamera = Camera.main;
+
+        Vector3 localPos = mainCamera.transform.InverseTransformPoint(transform.position);
+        float dist = localPos.z;
+        screenClamp.LeftBottom = 
+            mainCamera.ViewportToWorldPoint(new Vector3(0, 0, dist));
+        screenClamp.RightTop = 
+            mainCamera.ViewportToWorldPoint(new Vector3(1, 1, dist));
+        screenClamp.LeftBottom = 
+            mainCamera.transform.InverseTransformPoint(screenClamp.LeftBottom);
+        screenClamp.RightTop = 
+            mainCamera.transform.InverseTransformPoint(screenClamp.RightTop);
+
+        screenClamp.LeftBottom.x += padding.x;
+        screenClamp.LeftBottom.y += padding.x;
+        screenClamp.RightTop.x -= padding.x;
+        screenClamp.RightTop.y -= padding.y;
+    }
+
+    private struct ScreenClamp
+    {
+        public Vector3 LeftBottom;
+        public Vector3 RightTop;
+    }
 }
