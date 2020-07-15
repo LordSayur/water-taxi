f07dcbdfb9f9797927aafe2478ba339c6e337d61
Lord Sayur A <umar_317@hotmail.com>
2020-07-15 08:24:30 +0800

    Implement Player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed = 10f;
+
+    private Vector2 movementDirection;
+
+    private void Update()
+    {
+        Move(movementDirection.y, movementDirection.x, movementSpeed);
+    }
+
+    public void GetAxis(InputAction.CallbackContext context)
+    {
+        movementDirection = context.ReadValue<Vector2>();
+    }
+
+    private void Move(float forward, float sideway, float speed)
+    {
+        transform.position = new Vector3(
+                    transform.position.x + sideway * speed * Time.deltaTime,
+                    transform.position.y,
+                    transform.position.z + forward * speed * Time.deltaTime
+                    );
+    }
+
+}
