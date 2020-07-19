87637580fdf5be1dcdd0e001587c93a2c537ee96
Lord Sayur A <umar_317@hotmail.com>
2020-07-19 08:00:34 +0800

    Implement destroy on hit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private int _health = 200;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ProcessHit(other);
+    }
+
+    private void ProcessHit(Collider other)
+    {
+        var damageDealer = other.GetComponent<DamageDealer>();
+        _health -= damageDealer.GetDamage();
+
+        if (_health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
