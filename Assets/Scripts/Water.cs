f2736855c6c26c1534cb73e83377a1098b41012c
Lord Sayur A <umar_317@hotmail.com>
2020-07-21 06:35:41 +0800

    Create wave effect to the ground

---
+++ Assets/Scripts/Water.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class Water : MonoBehaviour
+{
+    [SerializeField]
+    private float _waveSpeed = 0.5f;
+    private MeshRenderer _meshRenderer;
+
+    void Start()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    void Update()
+    {
+        _meshRenderer.sharedMaterial.SetTextureOffset(1, new Vector2(_waveSpeed * Time.time, _waveSpeed * Time.time));
+    }
+}
