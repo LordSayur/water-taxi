5963bcd2a07db6efccd98bdce1378a11adfac959
Lord Sayur A <umar_317@hotmail.com>
2020-07-24 07:23:40 +0800

    Create Level manager script to Game Manager

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadGameOverScene()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+}
